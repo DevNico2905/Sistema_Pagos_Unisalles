0e0e579e18e9b3995064d59fef88f2fc342507a3
Nicolas Bernal <118196985+DevNico2905@users.noreply.github.com>
2025-05-22 22:59:06 -0500

    Desarrollo de los dashboards

--- sistema-pagos-frontend-unisalle/src/app/app-routing.module.ts
+++ sistema-pagos-frontend-unisalle/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LoadEstudiantesComponent } from './load-estudiantes/load-estudiantes.component';
+import { LoadPagosComponent } from './load-pagos/load-pagos.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EstudiantesComponent } from './estudiantes/estudiantes.component';
+import { PagosComponent } from './pagos/pagos.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:HomeComponent},
+  {path:"profile", component:ProfileComponent},
+  {path:"login", component:LoginComponent},
+  {path:"loadEstudiantes", component:LoadEstudiantesComponent},
+  {path:"loadPagos", component:LoadPagosComponent},
+  {path:"dashboard", component:DashboardComponent},
+  {path:"estudiantes", component:EstudiantesComponent},
+  {path:"pagos", component:PagosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
